2c9daee95cbe144610cdb7c7f169996479ab4d3c
RobertoVidaSoota <roberto50005000@gmail.com>
2021-03-22 20:11:35 -0300

    22-03-2021.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'carrinho',
+    loadChildren: () => import('./pages/carrinho/carrinho.module').then( m => m.CarrinhoPageModule)
   }
 ];
 @NgModule({
